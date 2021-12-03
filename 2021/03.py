7197200c19b3b5a7a897223900d9d4993a68bb7e
biscuitsnake <11334474+biscuitsnake@users.noreply.github.com>
2021-12-03 17:13:09 +0000

    day 3

---
+++ 2021/03.py
@@ -0,0 +1,44 @@
+from collections import Counter
+
+puzzle = [i.strip() for i in open("03.txt").readlines()]
+lp = len(puzzle)
+
+# Part 1:
+
+gr = ""
+for i in range(len(puzzle[0])):
+    c1 = sum([int(j[i]) for j in puzzle])
+    if c1 > lp//2:
+        gr += "1"
+    else:
+        gr += "0"
+
+gr = int(gr, 2)
+er = int("1"*len(puzzle[0]), 2) ^ gr
+
+print(gr*er)
+
+# Part 2:
+def rating(diagnostic, oxygen, ind):
+    c = Counter([int(j[ind]) for j in diagnostic])
+    mcv = max(c, key=c.get)
+
+    if not oxygen:
+        mcv = mcv ^ 1
+    if c[0] == c[1]:
+        if oxygen:
+            mcv = 1
+        else:
+            mcv = 0
+
+    diagnostic = [n for n in diagnostic if n[ind] == str(mcv)]
+
+    if len(diagnostic) == 1:
+        return diagnostic[0]
+    else:
+        return rating(diagnostic, oxygen, ind+1)
+
+ogr = rating(puzzle, True, 0)
+csr = rating(puzzle, False, 0)
+
+print(int(ogr, 2) * int(csr, 2))
